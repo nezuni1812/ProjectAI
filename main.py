454d13dd7e268092f1108dfca9a3b1317b896c78
nezuni1812 <tranngocuyennhi1812@gmail.com>
2024-07-24 20:40:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,37 @@
 import PathFinder
 import Visualizer
+import ReadInput
 
 def main():
-    maze_grid = [
-        [0, 0, 0, 0, -1, -1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, -1, 0, -1],
-        [0, 0, -1, -1, -1, 0, 0, -1, 0, -1],
-        [0, 0, 0, 0, -1, 0, 0, -1, 0, 0],
-        [0, 0, -1, -1, -1, 0, 0, -1, -1, 0],
-        [1, 0, -1, 0, 0, 0, 0, 0, -1, 0],
-        [0, 0, -2, 0, -1, 4, -1, 8, -1, 0],
-        [0, 0, 0, 0, -1, 0, 0, 0, 0, 0],
-        [0, -1, -1, -1, -1, 0, 0, 0, 0, 0],
-        [0, 0, 5, 0, 0, 0, -1, -1, -1, 0]
-    ]
+    file_path = 'input2_level2.txt'
+    n, m, time_limit, fuel_capacity, maze_grid, positions = ReadInput.read_input_file(file_path)
 
+    start = positions['S']  # Starting point 'S'
+    goal = positions['G']  # Goal point 'G'
+    
     visualizer = Visualizer.Visualizer()
 
-    start = (1, 1)
-    goal = (7, 8)
-
     # Level 1 Test 
-    bfs_finder  = PathFinder.BFSPathFinder(maze_grid, visualizer)
-    dfs_finder  = PathFinder.DFSPathFinder(maze_grid, visualizer)
-    ucs_finder  = PathFinder.UCSPathFinder(maze_grid, visualizer)
-    gbfs_finder = PathFinder.GBFSPathFinder(maze_grid, visualizer)
-    a_star_finder = PathFinder.AStarPathFinder(maze_grid, visualizer)
+    # bfs_finder  = PathFinder.BFSPathFinder(maze_grid, visualizer)
+    # dfs_finder  = PathFinder.DFSPathFinder(maze_grid, visualizer)
+    # ucs_finder  = PathFinder.UCSPathFinder(maze_grid, visualizer)
+    # gbfs_finder = PathFinder.GBFSPathFinder(maze_grid, visualizer)
+    # a_star_finder = PathFinder.AStarPathFinder(maze_grid, visualizer)
 
-    print("Running BFS...")
-    bfs_finder.start_visualizer(start, goal)
+    # print("Running BFS...")
+    # bfs_finder.start_visualizer(start, goal)
 
-    print("\nRunning DFS...")
-    dfs_finder.start_visualizer(start, goal)
+    # print("\nRunning DFS...")
+    # dfs_finder.start_visualizer(start, goal)
 
-    print("\nRunning UCS...")
-    ucs_finder.start_visualizer(start, goal)
+    # print("\nRunning UCS...")
+    # ucs_finder.start_visualizer(start, goal)
 
-    print("\nRunning GBFS...")
-    gbfs_finder.start_visualizer(start, goal)
+    # print("\nRunning GBFS...")
+    # gbfs_finder.start_visualizer(start, goal)
 
-    print("\nRunning A*...")
-    a_star_finder.start_visualizer(start, goal)
+    # print("\nRunning A*...")
+    # a_star_finder.start_visualizer(start, goal)
 
     # Level 2 Test
     level2_finder = PathFinder.PathFinderLevel2(maze_grid, visualizer)
