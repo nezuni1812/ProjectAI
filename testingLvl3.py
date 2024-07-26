d4e3df244ce2d62b06ca907914aeb352cf04a35c
nezuni1812 <tranngocuyennhi1812@gmail.com>
2024-07-26 07:40:20 +0700

    more obvious visualize

--- testingLvl3.py
+++ testingLvl3.py
@@ -3,12 +3,12 @@ import level3
 import ReadInput
 
 file_path = 'input.txt'
-n, m, time_limit, fuel_capacity, maze, positions = ReadInput.read_input_file(file_path)
+n, m, time_limit, fuel_capacity, raw_maze, maze, positions = ReadInput.read_input_file(file_path)
 
 start = positions['S']  # Starting point 'S'
 goal = positions['G']  # Goal point 'G'
 path = level3.a_star_fuel(start, goal, time_limit, fuel_capacity, maze)
-visualizer = Visualizer.Visualizer(maze)
+visualizer = Visualizer.Visualizer(raw_maze)
 if path:
     print("Path found:", path)
     total_cost = sum(level3.cost_to_move() for i in range(len(path)-1))
